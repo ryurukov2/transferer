e9dcd31c373411f426f66aa0c02215a10cbda330
Radoslav Yurukov <radoslav.vy@gmail.com>
2024-10-27 16:37:09 +0200

    logging

--- common.go
+++ common.go
@@ -2,12 +2,38 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 )
 
 var messageDelim string = "\r\n"
+var logDir string = "logs/"
+
+func setLogDir() error {
+	stat, err := os.Stat(logDir)
+	// 3. file/dir doesn't exist
+	if err != nil && os.IsNotExist(err) {
+		err := os.Mkdir(logDir, 0777)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	// 2. file exists but is not dir
+	if err == nil && !stat.IsDir() {
+		logDir := logDir + "(1)"
+		setReceivedFilesDir(logDir)
+	}
+	// 4. permission error
+	if stat.IsDir() && os.IsPermission(err) {
+		logDir := logDir + "(1)"
+		setReceivedFilesDir(logDir)
+	}
+	return nil
+}
 
 // reads files from an open directory and formats them to a string, deliminated by two commas.
 // Returns "filename//isDir" for all files.
@@ -42,4 +68,17 @@ func parseFileStr(fileStr string) ([]fileData, error) {
 		filesObj = append(filesObj, tmpObj)
 	}
 	return filesObj, nil
+}
+
+func writeLog(filename string, logdata string) {
+	fPath := filepath.Join("logs/", filename)
+	f, err := os.OpenFile(fPath,
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println(err)
+	}
+	defer f.Close()
+
+	logger := log.New(f, "", log.LstdFlags)
+	logger.Println(logdata)
 }
